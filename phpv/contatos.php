cec7d91379583d84af43401b18740d84e33d2f0f
wpdcastro <wpdcastro@gmail.com>
2017-09-05 13:50:28 -0300

    |Readme|

--- phpv/contatos.php
+++ phpv/contatos.php
@@ -35,50 +35,47 @@ if ((!empty($_POST['nome'])) && (!empty($_POST['email'])) &&
 	// Inicia a classe PHPMailer
 	$mail = new PHPMailer(true);
 	// Define os dados do servidor e tipo de conexão
-	// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-	// $mail->SMTPDebug = 2; É O DEBUG     
-	$mail->IsSMTP(true); // Define que a mensagem será SMTP
-	$mail->Host = "smtp.gmail.com"; // Endereço do servidor SMTP
-	$mail->SMTPAuth = true; // Usa autenticação SMTP? (opcional)
-	$mail->Username = 'wcastro@traylabs.com.br'; // Usuário do servidor SMTP
-	$mail->Password = 'vaigoku1'; // Senha do servidor SMTP
-	$mail->SMTPSecure = 'tls';     
-	$mail->Port = 587;
+	// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=  
+		$mail->IsSMTP(true); // Define que a mensagem será SMTP
+		$mail->Host = "smtp.gmail.com"; // Endereço do servidor SMTP
+		$mail->SMTPAuth = true; // Usa autenticação SMTP? (opcional)
+		$mail->Username = 'wcastro@traylabs.com.br'; // Usuário do servidor SMTP
+		$mail->Password = 'vaigoku1'; // Senha do servidor SMTP
+		$mail->SMTPSecure = 'tls';     
+		$mail->Port = 587;
 
- 
-    $mail->setFrom($_POST['email'], $_POST['name']);
-    // $mail->addAddress('msantos@traylabs.com.br', 'Marcelo Santos');
+	 
 
-	// Define o remetente
-	// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-	// $mail->From = $_POST['email']; // Seu e-mail
-	// $mail->FromName = $_POST['nome']; // Seu nome
-	// Define os destinatário(s)
-	// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-	$mail->AddAddress('wcastro@traylabs.com.br', 'Adm');
-	// Define os dados técnicos da Mensagem
-	// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-	$mail->IsHTML(true); // Define que o e-mail será enviado como HTML
-	// Define a mensagem (Texto e Assunto)
-	// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-	$mail->Subject  = $_POST['assunto']; // Assunto da mensagem
-	$mail->Body = $_POST['conteudo'];
-	$mail->AltBody = "Este é o corpo da mensagem de teste, em Texto Plano! \r\n :)";
-	// Define os anexos (opcional)
-	// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-	//$mail->AddAttachment("c:/temp/documento.pdf", "novo_nome.pdf");  // Insere um anexo
-	// Envia o e-mail
-	$enviado = $mail->Send();
-	// Limpa os destinatários e os anexos
-	$mail->ClearAllRecipients();
-	$mail->ClearAttachments();
-	// Exibe uma mensagem de resultado
-	if ($enviado) {
-	  echo "E-mail enviado com sucesso!";
-	} else {
-	  echo "Não foi possível enviar o e-mail.";
-	  echo "<b>Informações do erro:</b> " . $mail->ErrorInfo;
-	}
+
+		// Define o remetente
+		// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+		$mail->setFrom($_POST['email'], $_POST['name']);
+		// Define os destinatário(s)
+		// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+		$mail->AddAddress('wcastro@traylabs.com.br', 'Adm');
+		// Define os dados técnicos da Mensagem
+		// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+		$mail->IsHTML(true); // Define que o e-mail será enviado como HTML
+		// Define a mensagem (Texto e Assunto)
+		// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+		$mail->Subject  = $_POST['assunto']; // Assunto da mensagem
+		$mail->Body = $_POST['conteudo'];
+		$mail->AltBody = "Este é o corpo da mensagem de teste, em Texto Plano! \r\n :)";
+		// Define os anexos (opcional)
+		// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+		//$mail->AddAttachment("c:/temp/documento.pdf", "novo_nome.pdf");  // Insere um anexo
+		// Envia o e-mail
+		$enviado = $mail->Send();
+		// Limpa os destinatários e os anexos
+		$mail->ClearAllRecipients();
+		$mail->ClearAttachments();
+		// Exibe uma mensagem de resultado
+		if ($enviado) {
+		  echo "E-mail enviado com sucesso!";
+		} else {
+		  echo "Não foi possível enviar o e-mail.";
+		  echo "<b>Informações do erro:</b> " . $mail->ErrorInfo;
+		}
 }
 ?> 
 
